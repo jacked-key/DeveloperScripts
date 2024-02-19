b636bea02a670ed551326efd18cb0cee9240d349
Jackie Zheng <jackyzheng.zheng@gmail.com>
2024-02-18 17:28:48 -0800

    feat: made script directory agnostic and added vegas trip

--- tmux/template.sh
+++ tmux/template.sh
@@ -3,9 +3,11 @@
 set -eux
 
 export tmux_session="SessionName"
-source ./utility/tmux_window_split.sh
+# Get the directory where the script is located
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "$SCRIPT_DIR/utility/tmux_window_split.sh"
 
 BASE_DIR="/path/to/dir"
 
-standard_split 'NameOfFirstWindow' "${BASE_DIR}/dir1/"
+standard_split 'NameOfFirstWindow' "${BASE_DIR}/dir2/"
 box_split 'NameOfSecondWindow' "${BASE_DIR}/dir2/"
