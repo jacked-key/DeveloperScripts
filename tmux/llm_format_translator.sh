a68155966509549ed1cdf27212cf74dde7fc1667
Jackie Zheng <jackyzheng.zheng@gmail.com>
2024-08-25 12:39:09 -0700

    added new environment

--- tmux/llm_format_translator.sh
+++ tmux/llm_format_translator.sh
@@ -1,15 +1,14 @@
-
 #!/usr/bin/bash
 
 set -eux
 
-export tmux_session="LivingAgents"
+export tmux_session="LlmFormatTranslator"
 # Get the directory where the script is located
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/utility/tmux_window_split.sh"
 
-BASE_DIR=~/workspace/LivingAgents
+BASE_DIR=~/workspace/LlmFormatTranslator
 
-standard_split 'SourceCode' "${BASE_DIR}/LivingAgentsSourceCode/"
+standard_split 'SourceCode' "${BASE_DIR}/LlmFormatTranslator/"
 
 tmux a -t $tmux_session
